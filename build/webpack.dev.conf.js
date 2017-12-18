3b9f700b4fb76368950fba0a6231f31147bee463
zhengxinxin <zhengxinxin@b.360.cn>
2017-12-18 18:37:29 +0800

    all

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: 'dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin([path.resolve(__dirname, '../dist')]),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'url-loader'
+            ]
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                'url-loader'
+            ]
+        }]
+    }
+};
